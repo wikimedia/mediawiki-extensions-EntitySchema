c98ee7f454d5aea7f2835fb105bbcd53c51b8d77
Michael Große <michael.grosse@wikimedia.de>
2019-01-23 15:25:10 +0100

    Add incremental ID generator
    It was decided that we need an incrementing id for the Schemas.
    
    Bug: T213727
    Change-Id: I48f8400f0ce3b063fabad099b2de6a8bf00236f6

---
+++ src/WikibaseSchemaHooks.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Wikibase\Schema;
+
+use DatabaseUpdater;
+
+/**
+ * Hooks utilized by the WikibaseSchema extension
+ */
+final class WikibaseSchemaHooks {
+
+	/**
+	 * @param DatabaseUpdater $updater
+	 */
+	public static function onCreateDBSchema( DatabaseUpdater $updater ) {
+		$updater->addExtensionTable(
+			'wbschema_id_counter',
+			__DIR__ . '/../sql/WikibaseSchema.sql'
+		);
+	}
+
+}
