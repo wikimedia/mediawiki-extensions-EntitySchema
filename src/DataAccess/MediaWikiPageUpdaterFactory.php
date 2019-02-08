e3080ae7fea4ae323ed25a9ea8976dcb89d71f08
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2019-02-08 11:00:30 +0000

    Introduce SchemaWriter for writing schemas in a lightweight way
    Bug: T215516
    Change-Id: If757ff78743205f3f3f116520bce9293973bdc8c

---
+++ src/DataAccess/MediaWikiPageUpdaterFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wikibase\Schema\DataAccess;
+
+use MediaWiki\Storage\PageUpdater;
+use Title;
+use User;
+use WikiPage;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class MediaWikiPageUpdaterFactory {
+
+	private $user;
+
+	public function __construct( User $user ) {
+		$this->user = $user;
+	}
+
+	public function getPageUpdater( $pageTitleString ): PageUpdater {
+		$title = Title::makeTitle( NS_WBSCHEMA_JSON, $pageTitleString );
+		$wikipage = WikiPage::factory( $title );
+		return $wikipage->newPageUpdater( $this->user );
+	}
+
+}
