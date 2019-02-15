6d3b9ac56da91de99448c50ede737f161a3f74f8
Michael Große <michael.grosse@wikimedia.de>
2019-02-15 14:29:22 +0100

    Implement undo
    Bug: T214914
    Change-Id: I8bedc9aaf6280e83acc3decf33c1e8b7391b9d2a

---
+++ src/MediaWiki/Actions/UndoSubmitAction.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Wikibase\Schema\MediaWiki\Actions;
+
+use MediaWiki\MediaWikiServices;
+use PermissionsError;
+use ReadOnlyError;
+use RuntimeException;
+use Status;
+use UserBlockedError;
+use Wikibase\Schema\DataAccess\MediaWikiPageUpdaterFactory;
+use Wikibase\Schema\DataAccess\MediaWikiRevisionSchemaWriter;
+use Wikibase\Schema\DataAccess\SqlIdGenerator;
+use Wikibase\Schema\Domain\Model\SchemaId;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class UndoSubmitAction extends AbstractUndoAction {
+
+	public function getName() {
+		return 'submit';
+	}
+
+	public function getRestriction() {
+		return 'edit';
+	}
+
+	public function show() {
+		$permStatus = $this->checkPermissions();
+		if ( !$permStatus->isOK() ) {
+			$this->showUndoErrorPage( $permStatus );
+			return;
+		}
+
+		$undoStatus = $this->undo();
+		if ( !$undoStatus->isOK() ) {
+			$this->showUndoErrorPage( $undoStatus );
+		}
+
+		$this->getOutput()->redirect(
+			$this->getTitle()->getFullURL()
+		);
+	}
+
+	/**
+	 * @throws ReadOnlyError
+	 * @throws UserBlockedError
+	 * @throws PermissionsError
+	 */
+	private function checkPermissions(): Status {
+		$method = $this->getContext()->getRequest()->getMethod();
+		if ( $method !== 'POST' ) {
+			return Status::newFatal( 'wikibaseschema-error-not-post' );
+		}
+
+		if ( $this->getUser()->isBlockedFrom( $this->getTitle() ) ) {
+			throw new UserBlockedError( $this->getUser()->getBlock() );
+		}
+
+		if ( wfReadOnly() ) {
+			throw new ReadOnlyError;
+		}
+
+		if ( !$this->getUser()->isAllowed( $this->getRestriction() ) ) {
+			throw new PermissionsError( $this->getRestriction() );
+		}
+
+		return Status::newGood();
+	}
+
+	private function undo(): Status {
+		$req = $this->context->getRequest();
+
+		$diffStatus = $this->getDiffFromRequest( $req );
+		if ( !$diffStatus->isOK() ) {
+			return $diffStatus;
+		}
+
+		$patchStatus = $this->tryPatching( $diffStatus->getValue() );
+		if ( !$patchStatus->isOK() ) {
+			return $patchStatus;
+		}
+
+		return $this->storePatchedSchema( $patchStatus->getValue() );
+	}
+
+	private function storePatchedSchema( $patchedSchema ): Status {
+		$schemaWriter = new MediawikiRevisionSchemaWriter(
+			new MediaWikiPageUpdaterFactory( $this->getUser() ),
+			$this,
+			new SqlIdGenerator(
+				MediaWikiServices::getInstance()->getDBLoadBalancer(),
+				'wbschema_id_counter'
+			)
+		);
+
+		try {
+			$schemaWriter->updateSchema(
+				new SchemaId( $this->getTitle()->getTitleValue()->getText() ),
+				'en',
+				isset( $patchedSchema['labels'] ) ? $patchedSchema['labels']['en'] : '',
+				isset( $patchedSchema['descriptions'] ) ? $patchedSchema['descriptions']['en'] : '',
+				isset( $patchedSchema['aliases'] ) ? $patchedSchema['aliases']['en'] : [],
+				$patchedSchema['schema'] ?? ''
+			);
+		} catch ( RuntimeException $e ) {
+			return Status::newFatal( 'wikibaseschema-error-saving-failed', $e->getMessage() );
+		}
+
+		return Status::newGood();
+	}
+
+}
---
+++ tests/phpunit/MediaWiki/Actions/UndoViewActionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Wikibase\Schema\Tests\MediaWiki\Actions;
+
+use CommentStoreComment;
+use FauxRequest;
+use MediaWiki\Revision\SlotRecord;
+use MediaWikiTestCase;
+use RequestContext;
+use TextSlotDiffRenderer;
+use Title;
+use Wikibase\Schema\MediaWiki\Actions\UndoViewAction;
+use Wikibase\Schema\MediaWiki\Content\WikibaseSchemaContent;
+use Wikibase\Schema\MediaWiki\Content\WikibaseSchemaSlotDiffRenderer;
+use WikiPage;
+
+/**
+ * @license GPL-2.0-or-later
+ *
+ * @covers \Wikibase\Schema\MediaWiki\Actions\UndoViewAction
+ */
+class UndoViewActionTest extends MediaWikiTestCase {
+
+	public function test_UndoView() {
+		// arrange
+		$page = WikiPage::factory( Title::makeTitle( NS_WBSCHEMA_JSON, 'O123' ) );
+
+		$firstID = $this->saveSchemaPageContent( $page, [ 'schema' => 'abc' ] );
+		$secondId = $this->saveSchemaPageContent( $page, [ 'schema' => 'def' ] );
+
+		$context = RequestContext::getMain();
+		$context->setWikiPage( $page );
+		$context->setRequest(
+			new FauxRequest(
+				[
+					'action' => 'edit',
+					'undoafter' => $firstID,
+					'undo' => $secondId,
+				],
+				false
+			)
+		);
+
+		$textSlotDiffRenderer = new TextSlotDiffRenderer();
+		$textSlotDiffRenderer->setEngine( TextSlotDiffRenderer::ENGINE_PHP );
+		$diffRenderer = new WikibaseSchemaSlotDiffRenderer(
+			$context,
+			$textSlotDiffRenderer
+		);
+		$undoViewAction = new UndoViewAction( $page, $diffRenderer, $context );
+
+		// act
+		$undoViewAction->show();
+
+		// assert
+		$actualHTML = $undoViewAction->getContext()->getOutput()->getHTML();
+		$this->assertContains( '<ins class="diffchange diffchange-inline">abc</ins>', $actualHTML );
+		$this->assertContains( '<del class="diffchange diffchange-inline">def</del>', $actualHTML );
+	}
+
+	private function saveSchemaPageContent( WikiPage $page, array $content ) {
+		$updater = $page->newPageUpdater( self::getTestUser()->getUser() );
+		$updater->setContent( SlotRecord::MAIN, new WikibaseSchemaContent( json_encode( $content ) ) );
+		$firstRevRecord = $updater->saveRevision(
+			CommentStoreComment::newUnsavedComment(
+				'test summary 1'
+			)
+		);
+
+		return $firstRevRecord->getId();
+	}
+
+}
