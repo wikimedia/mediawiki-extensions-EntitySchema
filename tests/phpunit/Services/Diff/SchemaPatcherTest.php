c836a9ce0b5217ae8c54a7723df2577390884636
Michael Große <michael.grosse@wikimedia.de>
2019-02-15 19:23:27 +0100

    Add Schema patcher
    Apply a diff to a Schema and return the result.
    Throw an exception if the diff cannot be applied.
    
    Irritatingly, the null coalescing operate doesn't work on ArrayObjects
    in hhvm. It does just fine in PHP7 🙄
    That is the reason that the construct
    isset( $patch['labels'] ) ? $patch['labels'] : null
    is used in this patch.
    
    Bug: T214914
    Change-Id: I3f1cfaee5075e1df2298eb2b273f39d79690b4e4

---
+++ tests/phpunit/Services/Diff/SchemaPatcherTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Wikibase\Schema\Tests\Services\Diff;
+
+use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpChange;
+use Diff\DiffOp\DiffOpRemove;
+use Diff\Patcher\PatcherException;
+use PHPUnit\Framework\TestCase;
+use Wikibase\Schema\Services\Diff\SchemaPatcher;
+use Wikibase\Schema\Services\SchemaDispatcher\FullArraySchemaData;
+
+/**
+ * @license GPL-2.0-or-later
+ *
+ * @covers \Wikibase\Schema\Services\Diff\SchemaPatcher
+ * @covers \Wikibase\Schema\Services\Diff\AliasGroupListPatcher
+ */
+class SchemaPatcherTest extends TestCase {
+
+	use \PHPUnit4And6Compat;
+
+	public function provideValidSchemaPatches() {
+
+		yield 'restore label' => [
+			[],
+			new Diff( [
+				'labels' => new Diff( [
+					'en' => new DiffOpAdd( 'testlabel' ),
+				], true ),
+			], true ),
+			[
+				'labels' => [
+					'en' => 'testlabel',
+				],
+			],
+		];
+
+		$schemaEn = [
+			'labels' => [
+				'en' => 'test label',
+			],
+			'descriptions' => [
+				'en' => 'test description',
+			],
+			'aliases' => [
+				'en' => [ 'test alias', 'test alias 2' ],
+			],
+			'schema' => 'test schema',
+		];
+
+		yield 'changes, removals and additions' => [
+			$schemaEn,
+			new Diff( [
+				'labels' => new Diff( [
+					'en' => new DiffOpChange( 'test label', 'label for test' ),
+				], true ),
+				'descriptions' => new Diff( [
+					'en' => new DiffOpRemove( 'test description' ),
+					'de' => new DiffOpAdd( 'Testbeschreibung' ),
+				], true ),
+				'aliases' => new Diff( [
+					'en' => new Diff( [
+						new DiffOpAdd( 'test alias 3' ),
+						new DiffOpRemove( 'test alias 2' ),
+					], false ),
+					'de' => new Diff( [
+						new DiffOpAdd( 'Testalias' ),
+					], false ),
+				], true ),
+				'schema' => new DiffOpChange( 'test schema', 'schema for test' ),
+			], true ),
+			[
+				'labels' => [
+					'en' => 'label for test',
+				],
+				'descriptions' => [
+					'de' => 'Testbeschreibung',
+				],
+				'aliases' => [
+					'en' => [ 'test alias', 'test alias 3' ],
+					'de' => [ 'Testalias' ],
+				],
+				'schema' => 'schema for test',
+			],
+		];
+
+		yield 'restore schema' => [
+			[ 'schema' => '' ],
+			new Diff( [
+				'schema' => new DiffOpAdd( 'test schema' ),
+			], true ),
+			[ 'schema' => 'test schema' ],
+		];
+
+		yield 'remove schema' => [
+			[ 'schema' => 'test schema' ],
+			new Diff( [
+				'schema' => new DiffOpRemove( 'test schema' ),
+			], true ),
+			[],
+		];
+	}
+
+	/**
+	 * @dataProvider provideValidSchemaPatches
+	 */
+	public function testPatchSchema( $currentSchema, $patch, $expected ) {
+		$schemaPatcher = new SchemaPatcher();
+
+		$actualPatched = $schemaPatcher->patchSchema( new FullArraySchemaData( $currentSchema ), $patch );
+
+		$this->assertEquals( $expected, $actualPatched );
+	}
+
+	public function providInvalidSchemaPatches() {
+		yield 'restore existing schema' => [
+			[ 'schema' => 'I exist!' ],
+			new Diff( [
+				'schema' => new DiffOpAdd( 'test schema' ),
+			], true ),
+		];
+
+		yield 'existing label' => [
+			[
+				'labels' => [
+					'en' => 'existing label',
+				],
+			],
+			new Diff( [
+				'labels' => new Diff( [
+					'en' => new DiffOpAdd( 'testlabel' ),
+				], true ),
+			], true ),
+		];
+
+		yield 'remove changed description' => [
+			[
+				'descriptions' => [
+					'en' => 'I am not the original anymore',
+				],
+			],
+			new Diff( [
+				'descriptions' => new Diff( [
+					'en' => new DiffOpRemove( 'original description' ),
+				], true ),
+			], true ),
+		];
+
+		yield 'try to revert changed label' => [
+			[
+				'labels' => [
+					'en' => 'actual current label',
+				],
+			],
+			new Diff( [
+				'labels' => new Diff( [
+					'en' => new DiffOpChange( 'unwanted label', 'value to which to revert' ),
+				], true ),
+			] ),
+		];
+	}
+
+	/**
+	 * @dataProvider providInvalidSchemaPatches
+	 */
+	public function testPatchSchemaFailure( $currentSchema, $patch ) {
+		$schemaPatcher = new SchemaPatcher();
+
+		$this->expectException( PatcherException::class );
+
+		$schemaPatcher->patchSchema( new FullArraySchemaData( $currentSchema ), $patch );
+	}
+
+}
