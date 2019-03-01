b08be00411a14ef0ea9059d8e95e2dc0d07bb364
Michael Große <michael.grosse@wikimedia.de>
2019-03-01 17:03:52 +0100

    Implement Undo via API
    Bug: T217250
    Change-Id: I5900f9aefaa8be012e5ce99646e9f264e733b3c1

--- src/MediaWiki/Content/WikibaseSchemaContentHandler.php
+++ src/MediaWiki/Content/WikibaseSchemaContentHandler.php
@@ -4,17 +4,22 @@ namespace Wikibase\Schema\MediaWiki\Content;
 
 use Action;
 use Article;
+use Content;
 use IContextSource;
 use JsonContentHandler;
+use LogicException;
 use Page;
 use RequestContext;
+use Revision;
 use SlotDiffRenderer;
-use Wikibase\Schema\MediaWiki\Actions\RestoreViewAction;
+use Wikibase\Schema\DataAccess\SchemaEncoder;
 use Wikibase\Schema\MediaWiki\Actions\RestoreSubmitAction;
+use Wikibase\Schema\MediaWiki\Actions\RestoreViewAction;
 use Wikibase\Schema\MediaWiki\Actions\UndoSubmitAction;
 use Wikibase\Schema\MediaWiki\Actions\UndoViewAction;
 use Wikibase\Schema\MediaWiki\SchemaEditAction;
 use Wikibase\Schema\MediaWiki\SchemaSubmitAction;
+use Wikibase\Schema\MediaWiki\UndoHandler;
 use WikiPage;
 
 /**
@@ -98,6 +103,57 @@ class WikibaseSchemaContentHandler extends JsonContentHandler {
 
 	public function supportsDirectApiEditing() {
 		return false;
+	}
+
+	/**
+	 * Get the Content object that needs to be saved in order to undo all revisions
+	 * between $undo and $undoafter. Revisions must belong to the same page,
+	 * must exist and must not be deleted.
+	 *
+	 * @since 1.32 accepts Content objects for all parameters instead of Revision objects.
+	 *  Passing Revision objects is deprecated.
+	 *
+	 * @param Revision|Content $base The current text
+	 * @param Revision|Content $undoFrom The content of the revision to undo
+	 * @param Revision|Content $undoTo Must be from an earlier revision than $undo
+	 * @param bool $undoIsLatest Set true if $undo is from the current revision (since 1.32)
+	 *
+	 * @return Content|false
+	 */
+	public function getUndoContent( $base, $undoFrom, $undoTo, $undoIsLatest = false ) {
+		$undoToContent = ( $undoTo instanceof Revision ) ? $undoTo->getContent() : $undoTo;
+		if ( $undoIsLatest ) {
+			return $undoToContent;
+		}
+
+		$baseContent = ( $base instanceof Revision ) ? $base->getContent() : $base;
+		$undoFromContent = ( $undoFrom instanceof Revision ) ? $undoFrom->getContent() : $undoFrom;
+
+		$undoHandler = new UndoHandler();
+		try {
+			$schemaId = $undoHandler->validateContentIds( $undoToContent, $undoFromContent, $baseContent );
+		} catch ( LogicException $e ) {
+			return false;
+		}
+
+		$diffStatus = $undoHandler->getDiffFromContents( $undoFromContent, $undoToContent );
+		if ( !$diffStatus->isOK() ) {
+			return false;
+		}
+
+		$patchStatus = $undoHandler->tryPatching( $diffStatus->getValue(), $baseContent );
+		if ( !$patchStatus->isOK() ) {
+			return false;
+		}
+		$patchedSchema = $patchStatus->getValue();
+
+		return new WikibaseSchemaContent( SchemaEncoder::getPersistentRepresentation(
+			$schemaId,
+			$patchedSchema['labels'],
+			$patchedSchema['descriptions'],
+			$patchedSchema['aliases'],
+			$patchedSchema['schema']
+		) );
 	}
 
 }
