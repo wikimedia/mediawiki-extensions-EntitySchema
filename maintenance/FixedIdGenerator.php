6d9453b2b51f644cffdec8a124dc82a8c6dd3e4e
Michael Große <michael.grosse@wikimedia.de>
2019-05-23 12:58:30 +0200

    Add maintenance script to create preexisting Schemas
    Change-Id: Icd221e830d01e70ff64754a417f749a135e1c87d
    (cherry picked from commit 77302fa31399d65a557778bdaa557792886c3f27)

---
+++ maintenance/FixedIdGenerator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EntitySchema;
+
+use EntitySchema\Domain\Storage\IdGenerator;
+
+/**
+ * Returns a constant fixed id
+ *
+ * This is inteded to be used in maintenance scripts to create some predefined initial EntitySchemas
+ *
+ * @license GPL-2.0-or-later
+ */
+class FixedIdGenerator implements IdGenerator {
+
+	private $fixId;
+
+	public function __construct( $fixId ) {
+		$this->fixId = $fixId;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getNewId() {
+		return $this->fixId;
+	}
+
+}
---
+++ maintenance/createPreexistingSchemas.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace EntitySchema;
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' )
+	: __DIR__ . '/../../../..';
+
+require_once $basePath . '/maintenance/Maintenance.php';
+require_once $basePath . '/extensions/EntitySchema/src/Domain/Storage/IdGenerator.php';
+require_once 'FixedIdGenerator.php';
+
+use EntitySchema\DataAccess\MediaWikiPageUpdaterFactory;
+use EntitySchema\DataAccess\MediaWikiRevisionSchemaInserter;
+use EntitySchema\DataAccess\WatchlistUpdater;
+use ExtensionRegistry;
+use Maintenance;
+use RuntimeException;
+use User;
+
+/**
+ * Maintenance script for creating preexisting EntitySchemas.
+ *
+ * @license GPL-2.0-or-later
+ */
+class CreatePreexistingSchemas extends Maintenance {
+
+	const LABEL = 'label';
+	const DESC = 'desc';
+	const ALIASES = 'aliases';
+	const SCHEMATEXT = 'schematext';
+
+	public function __construct() {
+		$this->addDescription(
+			'Create initial EntitySchemas. Prossibly not that useful beyond wikidata.'
+		);
+
+		parent::__construct();
+	}
+
+	public function execute() {
+		$user = User::newSystemUser( 'Maintenance script' );
+
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'EntitySchema' ) ) {
+			$this->fatalError(
+				"You need to have EntitySchema enabled in order to use this maintenance script!\n\n"
+			);
+		}
+
+		$entities = $this->getSchemasToCreate();
+
+		$this->output( "Starting import...\n\n" );
+
+		foreach ( $entities as $idString => $dataMap ) {
+			$this->createSchema( $idString, $dataMap, $user );
+		}
+
+		$this->output( "Import completed.\n" );
+	}
+
+	private function getSchemasToCreate(): array {
+		return [
+			'E1' => [
+				self::LABEL => 'ShExR',
+				self::DESC => 'Schema of ShEx',
+			],
+
+			'E2' => [
+				self::LABEL => 'Wikimedia',
+				self::DESC => 'Schema of Wikimedia projects in Wikidata',
+			],
+
+			'E3' => [
+				self::LABEL => 'Wikidata Item',
+				self::DESC => 'Schema of a Wikidata item',
+			],
+
+			'E4' => [
+				self::LABEL => 'Labels/Descriptions',
+				self::DESC => 'Schema of labels and descriptions',
+			],
+
+			'E5' => [
+				self::LABEL => 'Statement',
+				self::DESC => 'Schema of a Statement',
+			],
+
+			'E6' => [
+				self::LABEL => 'Language maps',
+				self::DESC => 'Schema for language mappings in Wikidata',
+			],
+
+			'E7' => [
+				self::LABEL => 'Citation',
+				self::DESC => 'Schema of a Citation',
+			],
+
+			'E8' => [
+				self::LABEL => 'External RDF',
+				self::DESC => 'Schema of a Citation',
+			],
+
+			'E9' => [
+				self::LABEL => 'Wikidata-Wikibase',
+				self::DESC => 'Schema linking wikibase and wikidata',
+			],
+
+			'E40' => [
+				self::LABEL => 'Routes',
+				self::DESC => 'European routes',
+			],
+
+			'E42' => [
+				self::LABEL => 'author',
+				self::DESC => 'A Schema for authors',
+			],
+
+			'E123' => [
+				self::LABEL => 'Sandbox Schema',
+				self::DESC => 'An EntitySchema to try things out, not intended for productive use',
+			],
+
+			'E570' => [
+				self::LABEL => 'recently deceased humans',
+				self::DESC => 'Schema for humans who died recently',
+			],
+
+			'E734' => [
+				self::LABEL => 'family name',
+				self::DESC => 'basic scheme for family name items',
+			],
+
+			'E735' => [
+				self::LABEL => 'given name',
+				self::DESC => 'basic schema for given name items',
+			],
+
+			'E999' => [
+				self::LABEL => 'B0rked',
+				self::DESC => 'Broken schema',
+
+			],
+
+			'E1234' => [
+				self::LABEL => 'Sandbox Schema',
+				self::DESC => 'An EntitySchema to try things out, not intended for productive use',
+			],
+
+			'E3300' => [
+				self::LABEL => 'human',
+				self::DESC => 'basic schema for instances of Q5',
+			],
+
+			'E11424' => [
+				self::LABEL => 'Films',
+				self::DESC => 'basic schema for films',
+			],
+
+			'E12345' => [
+				self::LABEL => 'Sandbox Schema',
+				self::DESC => 'An EntitySchema to try things out, not intended for productive use',
+			],
+
+		];
+	}
+
+	private function createSchema( $idString, array $dataMap, User $user ) {
+		$this->output(
+			'Importing Schema with label ' . $dataMap[self::LABEL] . " as EntitySchema $idString... \n"
+		);
+
+		$pageUpdaterFactory = new MediaWikiPageUpdaterFactory( $user );
+
+		$fixedIdGenerator = new FixedIdGenerator( (int)trim( $idString, 'E' ) );
+
+		$schemaInserter = new MediaWikiRevisionSchemaInserter(
+			$pageUpdaterFactory,
+			new WatchlistUpdater( $user, NS_ENTITYSCHEMA_JSON ),
+			$fixedIdGenerator
+		);
+
+		try {
+			$schemaInserter->insertSchema(
+				'en',
+				$dataMap[self::LABEL] ?? '',
+				$dataMap[self::DESC] ?? '',
+				$dataMap[self::ALIASES] ?? [],
+				$dataMap[self::SCHEMATEXT] ?? ''
+			);
+		} catch ( RuntimeException $e ) {
+			$this->output(
+				'Failed to save ' . $dataMap[self::LABEL] . " with ID $idString. Moving on... \n"
+			);
+		}
+	}
+
+}
+
+$maintClass = CreatePreexistingSchemas::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
