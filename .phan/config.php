7d9ae82d09637a94e2229af59fbcf0591d1caf91
Umherirrender <umherirrender_de.wp@web.de>
2020-01-05 12:11:49 +0100

    build: Updating dependencies
    composer:
    * mediawiki/minus-x: 0.3.1 → 0.3.2
    * jakub-onderka/php-console-highlighter: 0.3.2 -> 0.4.0
    * mediawiki/mediawiki-phan-config: 0.8.0 -> 0.9.0
    
    npm:
    * grunt-banana-checker: 0.6.0 -> 0.8.1
    
    Change-Id: I1c767dcee4860b09b8d98f75d09358a6046ccf05

--- .phan/config.php
+++ .phan/config.php
@@ -2,4 +2,8 @@
 
 $upstreamConfig = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+// These are too spammy for now. TODO enable
+$upstreamConfig['null_casts_as_any_type'] = true;
+$upstreamConfig['scalar_implicit_cast'] = true;
+
 return $upstreamConfig;
