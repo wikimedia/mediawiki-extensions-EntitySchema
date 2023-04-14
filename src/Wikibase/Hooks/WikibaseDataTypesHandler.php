c6404a76038742aca4ba4275c010291a243850dd
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-04-14 15:32:09 +0200

    Set up initial validators for entity-schema data type
    Validate that the value looks syntactically valid. We don’t yet check
    that the referenced EntitySchema actually exists, that will be done
    separately.
    
    The maximum string length is chosen based on Int32EntityId::MAX (though
    that class is not actually related to SchemaId): ten decimal digits
    (enough to fit a 32-bit integer), plus one character for the ‘E’.
    
    Bug: T333813
    Change-Id: I89c9288fdf447c3a7da6d6abf8c7114fefe38402

--- src/Wikibase/Hooks/WikibaseDataTypesHandler.php
+++ src/Wikibase/Hooks/WikibaseDataTypesHandler.php
@@ -5,8 +5,12 @@ declare( strict_types = 1 );
 namespace EntitySchema\Wikibase\Hooks;
 
 use Config;
+use EntitySchema\Domain\Model\SchemaId;
 use EntitySchema\Wikibase\Formatters\EntitySchemaFormatter;
 use MediaWiki\Linker\LinkRenderer;
+use Wikibase\Repo\ValidatorBuilders;
+use Wikibase\Repo\Validators\DataValueValidator;
+use Wikibase\Repo\Validators\RegexValidator;
 
 /**
  * @license GPL-2.0-or-later
@@ -15,10 +19,16 @@ class WikibaseDataTypesHandler {
 
 	private LinkRenderer $linkRenderer;
 	public Config $settings;
+	private ValidatorBuilders $validatorBuilders;
 
-	public function __construct( LinkRenderer $linkRenderer, Config $settings ) {
+	public function __construct(
+		LinkRenderer $linkRenderer,
+		Config $settings,
+		ValidatorBuilders $validatorBuilders
+	) {
 		$this->linkRenderer = $linkRenderer;
 		$this->settings = $settings;
+		$this->validatorBuilders = $validatorBuilders;
 	}
 
 	public function onWikibaseRepoDataTypes( array &$dataTypeDefinitions ): void {
@@ -32,6 +42,11 @@ class WikibaseDataTypesHandler {
 					$format,
 					$this->linkRenderer
 				);
+			},
+			'validator-factory-callback' => function (): array {
+				$validators = $this->validatorBuilders->buildStringValidators( 11 );
+				$validators[] = new DataValueValidator( new RegexValidator( SchemaId::PATTERN ) );
+				return $validators;
 			},
 		];
 	}
--- tests/phpunit/unit/Wikibase/Hooks/WikibaseDataTypesHandlerTest.php
+++ tests/phpunit/unit/Wikibase/Hooks/WikibaseDataTypesHandlerTest.php
@@ -4,11 +4,14 @@ declare( strict_types = 1 );
 
 namespace phpunit\unit\Wikibase\Hooks;
 
+use DataValues\StringValue;
 use EntitySchema\Wikibase\Formatters\EntitySchemaFormatter;
 use EntitySchema\Wikibase\Hooks\WikibaseDataTypesHandler;
 use HashConfig;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWikiUnitTestCase;
+use Wikibase\Repo\ValidatorBuilders;
+use Wikibase\Repo\Validators\CompositeValidator;
 
 /**
  * @covers \EntitySchema\Wikibase\Hooks\WikibaseDataTypesHandler
@@ -21,8 +24,9 @@ class WikibaseDataTypesHandlerTest extends MediaWikiUnitTestCase {
 			'EntitySchemaEnableDatatype' => true,
 		] );
 		$stubLinkRenderer = $this->createStub( LinkRenderer::class );
+		$stubValidatorBuilders = $this->createStub( ValidatorBuilders::class );
 
-		$sut = new WikibaseDataTypesHandler( $stubLinkRenderer, $settings );
+		$sut = new WikibaseDataTypesHandler( $stubLinkRenderer, $settings, $stubValidatorBuilders );
 
 		$dataTypeDefinitions = [ 'PT:wikibase-item' => [] ];
 		$sut->onWikibaseRepoDataTypes( $dataTypeDefinitions );
@@ -40,12 +44,46 @@ class WikibaseDataTypesHandlerTest extends MediaWikiUnitTestCase {
 			'EntitySchemaEnableDatatype' => false,
 		] );
 		$stubLinkRenderer = $this->createStub( LinkRenderer::class );
+		$stubValidatorBuilders = $this->createStub( ValidatorBuilders::class );
 
-		$sut = new WikibaseDataTypesHandler( $stubLinkRenderer, $settings );
+		$sut = new WikibaseDataTypesHandler( $stubLinkRenderer, $settings, $stubValidatorBuilders );
 
 		$dataTypeDefinitions = [ 'PT:wikibase-item' => [] ];
 		$sut->onWikibaseRepoDataTypes( $dataTypeDefinitions );
 
 		$this->assertSame( [ 'PT:wikibase-item' => [] ], $dataTypeDefinitions );
 	}
+
+	/**
+	 * Basic test for validating an EntitySchema ID value based on the pattern.
+	 * Further test cases, especially invalid ones, require integration with Wikibase
+	 * (instead of stubbing ValidatorBuilders) and are tested in {@link EntitySchemaDataValidatorTest}.
+	 *
+	 * @dataProvider provideValuesWithValidity
+	 */
+	public function testOnWikibaseRepoDataTypesValidator( string $value, bool $isValid ): void {
+		$settings = new HashConfig( [
+			'EntitySchemaEnableDatatype' => true,
+		] );
+		$stubLinkRenderer = $this->createStub( LinkRenderer::class );
+		$validatorBuilders = $this->createConfiguredMock( ValidatorBuilders::class, [
+			'buildStringValidators' => [],
+		] );
+		$handler = new WikibaseDataTypesHandler( $stubLinkRenderer, $settings, $validatorBuilders );
+		$dataTypeDefinitions = [];
+		$handler->onWikibaseRepoDataTypes( $dataTypeDefinitions );
+		$validator = new CompositeValidator(
+			$dataTypeDefinitions['PT:entity-schema']['validator-factory-callback']()
+		);
+
+		$result = $validator->validate( new StringValue( $value ) );
+
+		$this->assertSame( $isValid, $result->isValid() );
+	}
+
+	public static function provideValuesWithValidity(): iterable {
+		yield 'valid' => [ 'E1', true ];
+		yield 'invalid' => [ '', false ];
+	}
+
 }
