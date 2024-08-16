045858d735babf3938a031a94bee28856cc7ab64
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2024-08-16 16:36:23 +0200

    Clone Wikibase submodules from GitHub
    Based on the equivalent workaround in Wikibase Cloud [1], as mentioned
    in T372458.
    
    [1]: https://github.com/wmde/wikibase-release-pipeline/commit/9d69632e47fff0b6133a2c7625ecea841cbdf3f7
    
    Bug: T372626
    Change-Id: I2037cd8bb5d568021472e048900649028b5dcc62

--- build/ci-scripts/install.sh
+++ build/ci-scripts/install.sh
@@ -20,6 +20,30 @@ git clone -b $MW_BRANCH https://gerrit.wikimedia.org/r/mediawiki/extensions/cldr
 git clone -b $MW_BRANCH https://gerrit.wikimedia.org/r/mediawiki/extensions/SyntaxHighlight_GeSHi --depth 1
 git clone -b $MW_BRANCH https://gerrit.wikimedia.org/r/mediawiki/extensions/Wikibase --depth 1
 cd Wikibase
+# work around GitHub Actions being blocked from Phabricator (T372626)
+git apply << 'EOF'
+diff --git a/.gitmodules b/.gitmodules
+index df41c768af..e9926d6ddd 100644
+--- a/.gitmodules
++++ b/.gitmodules
+@@ -3,13 +3,13 @@
+ 	url = https://gerrit.wikimedia.org/r/data-values/value-view
+ [submodule "view/lib/wikibase-serialization"]
+ 	path = view/lib/wikibase-serialization
+-	url = https://phabricator.wikimedia.org/source/wikibase-serialization.git
++	url = https://github.com/wmde/WikibaseSerializationJavaScript.git
+ [submodule "view/lib/wikibase-data-values"]
+ 	path = view/lib/wikibase-data-values
+-	url = https://phabricator.wikimedia.org/source/datavalues-javascript.git
++	url = https://github.com/wmde/DataValuesJavaScript.git
+ [submodule "view/lib/wikibase-data-model"]
+ 	path = view/lib/wikibase-data-model
+-	url = https://phabricator.wikimedia.org/source/wikibase-data-model.git
++	url = https://github.com/wmde/WikibaseDataModelJavaScript.git
+ [submodule "view/lib/wikibase-termbox"]
+ 	path = view/lib/wikibase-termbox
+ 	url = https://gerrit.wikimedia.org/r/wikibase/termbox
+EOF
 git submodule update --init
 cd .. # back to extensions/
 
