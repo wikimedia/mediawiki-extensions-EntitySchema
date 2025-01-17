85e53e78102c42ffd802dfc4a653b3d3e19b6bca
Cole White <cwhite@wikimedia.org>
2025-01-17 19:53:20 +0000

    send external checkschema metrics to Prometheus
    Bug: T359467
    Change-Id: I276d801496348b3326c1fcb5107dc29ef3b3a6e1

--- resources/action.view.trackclicks.js
+++ resources/action.view.trackclicks.js
@@ -3,6 +3,7 @@
 	$( () => {
 		$( '.entityschema-check-schema' ).on( 'click', () => {
 			mw.track( 'counter.MediaWiki.EntitySchema.external.checkSchema' );
+			mw.track( 'stats.mediawiki_EntitySchema_external_checkschema_total' );
 		} );
 	} );
 }() );
